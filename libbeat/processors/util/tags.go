616f905d9c716e5b9d049fe60c94ac995bb8573f
shuai.qi <shuai.qi@jiduauto.com>
2024-01-24 18:09:32 +0800

    chore: comments

--- libbeat/processors/util/tags.go
+++ libbeat/processors/util/tags.go
@@ -3,7 +3,7 @@ package util
 const (
 	BenchmarkPrefix = "00000000"
 	// BenchmarkExcludePrefix 吉利调用集度接口时，传递的trace_id以16位0开头，为了防止这种非
-	// 压测日志被过滤，特由此逻辑
+	// 压测日志被过滤，特由此逻辑。当前吉利请求集度的服务为：pdp-gateway
 	BenchmarkExcludePrefix = BenchmarkPrefix + BenchmarkPrefix
 	MsgTag                 = "##JIDU##"
 	MsgTagConcatenated     = "##JIDU####JIDU##"
